4db85159d0c8637345cbc409cff1de672f42718e
Wendi Zhang <wendizhang@berkeley.edu>
2020-04-30 17:53:09 -0700

    change cell color

--- WeatherPrediction/Controller/City Tab/WeatherViewController.swift
+++ WeatherPrediction/Controller/City Tab/WeatherViewController.swift
@@ -114,8 +114,26 @@ extension WeatherViewController: UICollectionViewDelegate, UICollectionViewDataS
             let format = DateFormatter()
             format.timeZone = TimeZone(secondsFromGMT: city.timezone)
             format.dateFormat = "h:mm a"
-            let dateString = format.string(from: currentDate)
             
+            let dateString = format.string(from: currentDate)
+            let dateArr = dateString.components(separatedBy: " ")
+            let dateTime = dateArr[0].components(separatedBy: ":")[0]
+            if dateArr[1] == "AM" {
+                
+                if (Int(dateTime)! >= 6) && (Int(dateTime)! < 12){
+                    cell.contentView.backgroundColor = UIColor(rgb: 0xE4B888)
+                        
+                } else {
+                    cell.contentView.backgroundColor = UIColor(rgb: 0xDD9863)
+                }
+            } else {
+                if (Int(dateTime)! >= 6) && (Int(dateTime)! < 12){
+                    cell.contentView.backgroundColor = UIColor(rgb: 0xAE7467)
+                } else {
+                    cell.contentView.backgroundColor = UIColor(rgb: 0x968788)
+                }
+            }
+                        
             cell.deleteButton?.tag = indexPath.row
             cell.deleteButton?.addTarget(self, action: #selector(deleteCity(sender:)), for: UIControl.Event.touchUpInside)
 
@@ -160,3 +178,21 @@ extension WeatherViewController {
     }
 }
 
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
--- WeatherPrediction/Controller/City Tab/detailWeatherViewController.swift
+++ WeatherPrediction/Controller/City Tab/detailWeatherViewController.swift
@@ -16,6 +16,8 @@ class detailWeatherViewController: UIViewController {
     @IBOutlet weak var temperature: UILabel!
     @IBOutlet weak var weather: UILabel!
     @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var feels_like: UILabel!
     @IBOutlet weak var sunrise: UILabel!
     @IBOutlet weak var sunset: UILabel!
     
@@ -27,14 +29,23 @@ class detailWeatherViewController: UIViewController {
         super.viewDidLoad()
         //Configure data on detail page
         if let city = selectedCity {
+            cityName.lineBreakMode = .byWordWrapping
             cityName.text = city.name
             weather.text = city.weatherDescription
-            if let humidty = city.weatherStat["humidity"] as? String {
-                 humidity.text = humidty
+            weather.textAlignment = .center
+            if let humidty = city.weatherStat["humidity"] as? Int {
+                 humidity.text = "Humidity \(humidty) %"
             }
-            weatherSymbol.image = UIImage(named: selectedCity!.weatherDescription)
+            if let pre = city.weatherStat["pressure"] as? Int {
+                 pressure.text = "Pressure \(pre) hPa"
+            }
+            if let feels = city.weatherStat["feels_like"] as? Double {
+                feels_like.text = "Feels like \(feels) °C"
+            }
+            weatherSymbol.image = UIImage(named: selectedCity!.imageDescription)
             if let temp = selectedCity!.weatherStat["temp"] as? Double {
                 temperature.text = String(temp)+"°C"
+                temperature.textAlignment = .center
             }
         }
     }
